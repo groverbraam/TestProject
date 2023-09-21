53e6cd07b4180482471be16d7b59042f84ad0018
Gover Braam <groverbraam@gmail.com>
2023-09-20 22:10:51 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿string[] orderID = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+foreach (string order in orderID)
+{
+    if (order.StartsWith("B"))
+    {
+        Console.WriteLine($"{order} starts with a B!");
+    }
+}
+
+
